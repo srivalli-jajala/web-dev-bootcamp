04624e8288b3ef3d7e77e93f3ebfcb8a04d67c96
Jajala Srivalli <jajalasrivalli0705@gmail.com>
2026-08-13 16:27:39 +0530

    toggling completed

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,30 @@
-alert("Welcome to Srivalli!");
+// alert("Welcome to Srivalli!");
+
+var users = [
+    {
+        "name": "John doe",
+        "gender": "Male",
+        "image":"john.png"
+    },
+    {
+        "name": "Jane doe",
+        "gender": "Female",
+        "image":"jane.png"
+    }
+]
+
+var curId = 0;
+
+function toggleUser(){
+    curId = (curId + 1) % 2;
+
+    var userName = document.getElementById("user-name");
+    var userGender = document.getElementById("user-gender");
+    var userImage = document.getElementById("user-image");
+
+    userName.innerHTML = users[curId].name;
+    userGender.innerHTML = users[curId].gender;
+    userImage.src = users[curId].image;
+}
+
+
